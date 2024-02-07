63204376fa2df28deae8bacb6e7ac75c42b273c0
naveganth <159157666+naveganth@users.noreply.github.com>
2024-02-06 21:36:28 -0300

    Update Untitled-1.sql

--- Untitled-1.sql
+++ Untitled-1.sql
@@ -1,9 +1,30 @@
--- Active: 1707262400693@@127.0.0.1@3306
+--MYSQL CODE
 
-CREATE TABLE test(cpf VARCHAR(255));
+--Codigo para inicializar a DATABASE
+mysql -u root -p
 
-INSERT INTO test(cpf) VALUES(cp)
+USE META;
 
-DROP DATABASE meta
+CREATE TABLE PESSOA (IDPESSOA INT NOT NULL, NOME VARCHAR(255), IDADE INT, PRIMARY KEY (IDPESSOA));
 
-DROP Table test
+SHOW TABLES;
+DESCRIBE PESSOA
+
+--Inserir valores em uma determinada tabelas seja ela pessoa, nome ou idade
+INSERT INTO pessoa VALUES(3, 'NOSFERATU', 200);
+
+--Usar para ver dados
+SELECT * FROM pessoa
+
+--Atualiza pessoas existentes na DATABASE
+UPDATE pessoa SET NOME = 'JOSEFINA' WHERE IDPESSOA=2;
+
+--Deletar linhas na TABLE
+DELETE FROM pessoa WHERE `IDPESSOA`=4;
+
+--Utilizar para fazer uma pesquisar por nome que possuem AR 
+SELECT * FROM pessoa WHERE `NOME` LIKE "%AR%";
+
+--Utilizar para deletar uma pessoa com base em nome idade ou id, utilizando o LIKE
+DELETE FROM pessoa WHERE `NOME` LIKE "%NOSFERA%";
+
